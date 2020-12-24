323c66d08827a8a5f6f610526344430998132a83
Fandor626 <58294921+Fandor626@users.noreply.github.com>
2020-12-24 13:07:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp186
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Strings stringg = new Strings();
+            stringg.OutPut();
+            //Nums num = new Nums();
+            //num.OutPut();
+            //SmallNums smallnum = new SmallNums();
+        }
+    }
+    public class Strings 
+    {
+        string str;
+        public double Length()
+        {
+            Input();
+            int Out =str.Length;
+            return Out;
+        }
+        public void Input()
+        {
+            str = "dfsdffxgdfsdf";
+        }
+        public void OutPut()
+        {
+            Console.WriteLine(Length());
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'f')
+                {
+                    str = str.Insert(++i,"...");
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+    public class Nums : Strings
+    {
+        string str;
+        public Nums(string Str) {str=Str; }
+        public new void Input ()
+        {
+            str = "12354895";
+        }
+        public new void OutPut()
+        {
+            Input();
+            Console.WriteLine(str.Length);
+        }
+    }
+    public class SmallNums : Strings
+    {
+        string strSmall;
+        public SmallNums(string Strsmall) { strSmall = Strsmall; }
+            public void input()
+        {
+            strSmall="fghfgh";
+        }   
+        public new void OutPut()
+        {
+            Console.WriteLine(strSmall.Length);
+        }
+
+    }
+}
